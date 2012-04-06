2bb45f76695e2f03adf8b51d9208ca45a96da4d2
Marco Palladino <marco@mashape.com>
2012-04-06 12:59:15 -0700

    v1

---
+++ README.md
@@ -0,0 +1,16 @@
+Mashape Python Client
+============================================
+
+This is the base library for Mashape client libraries.
+
+For questions or feedback visit [mashape.com](http://mashape.com/)
+
+Documentation
+-------------------
+
+For the documentation, please visit http://www.mashape.com/guide/consume/python
+
+Copyright
+---------------
+
+Copyright (C) 2012 Mashape, Inc.
--- mashape/http/auth_utils.py
+++ mashape/http/auth_utils.py
@@ -1,4 +1,4 @@
-import uuid
+
 import hmac
 import hashlib
 import base64
@@ -7,8 +7,7 @@ class AuthUtils:
 
 	@staticmethod
 	def generate_authentication_header(public_key, private_key):
-		newuuid = str(uuid.uuid4())
-		digest_maker = hmac.new(private_key, newuuid, hashlib.sha1)
-		uuid_hash = digest_maker.hexdigest()
-		return { "X-Mashape-Authorization" : base64.b64encode(public_key + ":" + uuid_hash + newuuid) }
+		digest_maker = hmac.new(private_key, public_key, hashlib.sha1)
+		hash = digest_maker.hexdigest()
+		return { "Proxy-Authorization" : base64.b64encode(public_key + ":" + hash) }
 
