489b69dc2581115009bb922140737e0563becbe1
thefosk <marco@mashape.com>
2013-04-23 14:34:56 -0700

    renaming pytohn

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from distutils.core import setup
 
 setup(
     name='Unirest',
-    version='1.0.2',
+    version='1.0.3',
     author='Mashape Inc.',
     author_email='support@mashape.com',
     packages=['unirest'],
-    url='http://pypi.python.org/pypi/Unirest/',
+    url='http://unirest.io',
     license='LICENSE.txt',
     description='Unirest, the lightweight HTTP library',
-    long_description=open('README.txt').read(),
     install_requires=[
         "poster >= 0.8.1"
     ],
