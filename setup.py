11ed07f337e1e59d86475590f44c22eb50de4926
thefosk <marco@mashape.com>
2013-05-29 16:29:06 -0700

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='Unirest',
-    version='1.0.4',
+    version='1.0.5',
     author='Mashape Inc.',
     author_email='support@mashape.com',
     packages=['unirest'],
