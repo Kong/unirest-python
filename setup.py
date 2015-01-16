1ae3ac6f74ffdca9c0a6f2cf78162e283a88e541
Ahmad Nassri <ahmad@codeinchaos.com>
2015-01-16 00:49:14 -0500

    updating contact info and renaming LICENSE.txt to LICENSE

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ except ImportError:
 setup(
     name='Unirest',
     version='1.1.6',
-    author='Mashape Inc.',
-    author_email='support@mashape.com',
+    author='Mashape',
+    author_email='opensource@mashape.com',
     packages=['unirest'],
     url='https://github.com/Mashape/unirest-python',
-    license='LICENSE.txt',
-    description='Unirest, the lightweight HTTP library',
+    license='LICENSE',
+    description='Simplified, lightweight HTTP client library',
     install_requires=[
         "poster >= 0.8.1"
     ]
